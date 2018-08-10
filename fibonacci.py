4ad7fc8a867efffe5b630eaa0da971bf6533b3d0
io99 <31932394+io99@users.noreply.github.com>
2018-08-10 09:32:59 -0700

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,48 @@
+# Given any whole number n
+
+# if n is odd - 3n+1 if n is even - n/2
+
+# I wrote some code to do this for any number.
+
+n = int(input("Enter an integer"))
+print(n)
+while n!= 1:
+	if(n%2 == 0):
+		n=n/2
+		print(n)
+	else:
+		n = (3*n)+1
+		print(n)
+
+
+
+n = int(input("Enter an integer"))
+print(n)
+while n!=1:
+	if(n%2 == 0):
+		n=n/2
+		print(n)
+	else:n =(3*n)+1
+	print(n)
+
+
+n = int(input("Enter an integer"))
+print(n)
+while n!=1:
+	if(n%2 == 0):
+		n=n/2
+		print(n)
+	else:
+		n=(3*n)+1
+		print(n)
+
+
+n = int(input("Enter an integer"))
+print(n)
+while n!=1:
+	if(n%2 ==0):
+		n=n/2
+		print(n)
+	else:
+		n = (3*n)+1
+		print(n)
