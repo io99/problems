cef05005ca221ab42c7c185e61691d0f1582902f
io99 <31932394+io99@users.noreply.github.com>
2018-08-13 10:41:35 -0700

    Add files via upload

--- practice.py
+++ practice.py
@@ -107,3 +107,36 @@ def is_unique(input_str):
 			return False
 
 			
+====================================================
+
+unique_str = "rnosd"
+non_unique_str = " idnkl sdino"
+
+input_str = input_str.replace(" ", "")
+return input_str.lower()
+
+def is_unique(input_str):
+	d = dict()
+	for i in input_str:
+		if i in d:
+			return False
+		else:
+			d[i] == 1
+			return True
+
+---------------------------------------
+
+unique_str = " ihkjbjk"
+non_unique_str = "yhjl, tugbhn  pijmm"
+
+input_str = input_str.replace(" ", "")
+return input_str.lower()
+
+def is_unique(input_str):
+	d = dict()
+	for i in input_str:
+		if i in d:
+			return False 
+		else:
+			d[i] == 1
+			return True
