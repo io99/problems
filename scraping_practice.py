55f61cebe0c4a436d670f7538fa888636219da2e
io99 <31932394+io99@users.noreply.github.com>
2018-08-17 08:02:27 -0700

    Add files via upload

--- scraping_practice.py
+++ scraping_practice.py
@@ -11,3 +11,18 @@ rating = soup.find('span', itemprop='ratingValue').get_text()
 print('Title: {}'.format(title)) # if using python 2, just print 'Title {}'.format(title) will work
 print('Summary: {}'.format(summary))
 print('Rating: {}'.format(rating))
+
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.imdb.com/title/tt0451279')
+soup = BeautifulSoup(r.text, 'html.parser')
+
+title = soup.h1.get_text()
+summary = soup.find('div', class = 'summary_text').get_text()
+rating = soup.find('span', itemprop= 'ratingValue').get_text()
+
+print(title)
+print(Summary)
+print(rating)
